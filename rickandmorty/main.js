e2999ebff8eeb109591d2fef8460b4a20464d697
treflest remy <mrtreflestremy@outlook.com>
2023-01-05 12:35:25 +0100

    ApiClient + MapperExtension

--- rickandmorty/main.js
+++ rickandmorty/main.js
@@ -2,9 +2,14 @@ import './style.css'
 import Card from "./src/components/Card";
 import UserPage from "./src/pages/UserPage";
 import TabManager from "./src/utils/TabManager";
+import ApiClient from "./src/utils/ApiClient";
+import { mapJsonToCharactersCard } from './src/utils/MapperExtension';
 
 const rootElement = document.querySelector('#app')
-
+const apiClient = new ApiClient();
+const datas = await apiClient.getCharactersPaginated(3);
+console.log(mapJsonToCharactersCard(datas.results));
+// console.log(apiClient.getCharactersFilteredByName('rick'));
 const tabManager = new TabManager(rootElement, {
   page1: {
     component: () => document.createElement('div'),
