f3194385f6fed77d4b4896dd4578ece36592c7dd
treflest remy <mrtreflestremy@outlook.com>
2023-01-06 16:36:28 +0100

    test unitaire

---
+++ rickandmorty/test/createElement.test.js
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest';
+import createDOMElement from '../src/dom/createDOMElement';
+import charactersList from '../src/pages/CharactersList';
+
+const tree = {
+    tagName: 'div',
+    text: "Hello World",
+    children: [
+        {
+          tagName: 'img',
+          text: "",
+          classList: ['card']
+        }
+      ],
+  };
+
+test('should create an element', () => {
+    const element = createDOMElement(tree);
+    expect(element.textContent).toContain("Hello World");
+    expect(element.querySelectorAll('.card').length).toEqual(1);
+});
